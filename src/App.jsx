eb5996390665ff3d41af7b16a9a0f732b96ca9f2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2025-02-16 09:14:29 +0530

    created Service component and add it in to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Service from './components/Service/Service'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Service/>
     </div>
   )
 }
