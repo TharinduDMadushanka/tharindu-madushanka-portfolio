267d9b6560d46d3ac45a275e8ebd84370add57dd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2025-02-25 20:08:20 +0530

    creatde MyService component and route it

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
+import MyServices from './components/MyServices/MyServices'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Service/>
       <About/>
       <Projects/>
+      <MyServices/>
       <Contact/>
       <Footer/>
     </div>
---
+++ src/components/MyServices/MyServices.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MyServices.css'
+
+const MyServices = () => {
+  return (
+    <div>
+      MyServices
+    </div>
+  )
+}
+
+export default MyServices
