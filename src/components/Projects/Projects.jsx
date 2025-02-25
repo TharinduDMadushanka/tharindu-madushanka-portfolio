372a481cb5d757fa634e3a5a14c4b9fab7a8ca63
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2025-02-25 12:13:37 +0530

    replace project images

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import "./Projects.css";
 import project from '../../assets/Projects/project1.jpg';
+import interview_suite from '../../assets/Projects/interview-suite.png';
+import blog_site from '../../assets/Projects/blog-site.jpg';
+import real_estate from '../../assets/Projects/real-estate.png';
+import library_ms from '../../assets/Projects/library-ms.jpg';
 
 const projects = [
   {
     title: "Interview Suite - AI Mock Interview Platform",
     description: "An AI-powered mock interview practice platform designed to help interns prepare for real-world interviews. Features include AI-generated questions, automated answer evaluation, and real-time feedback. Built using Python Flask for the backend and React.js for the frontend.",
-    image: project,  // Replace with actual project image
-    link: "#", // Replace with actual project link
+    image: interview_suite,  
+    link: "#", 
   },
   {
     title: "TechBlogs - A Blogging Platform",
     description: "A full-stack blog website that allows users to create, edit, and manage blog posts. Supports user authentication, rich-text editing, and SEO optimization. Built with Spring Boot for the backend and React.js for the frontend.",
-    image: project,  // Replace with actual project image
-    link: "#", // Replace with actual project link
+    image: blog_site, 
+    link: "#", 
   },
   {
     title: "EstateFinder - Real Estate Search App",
     description: "A modern real estate search application that enables users to find properties using advanced filtering options such as location, price range, property type, and amenities. Provides a seamless UI for an enhanced user experience.",
-    image: project,  // Replace with actual project image
-    link: "#", // Replace with actual project link
+    image: real_estate,  
+    link: "#", 
   },
   {
     title: "LibraPro - Library Management System",
     description: "A JavaFX-based library management system that streamlines book borrowing, returns, and inventory management. Built using a layered architecture in Java, ensuring maintainability and scalability.",
-    image: project,  // Replace with actual project image
-    link: "#", // Replace with actual project link
+    image: library_ms,  
+    link: "#", 
   },
 ];
 
